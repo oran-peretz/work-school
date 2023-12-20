6809799e7fbe5c5942714b36da7f9340ebefe772
oran-peretz <99199807+oran-peretz@users.noreply.github.com>
2023-12-20 13:59:51 +0200

    Update and rename file7.java to file7_8.java

--- file7_8.java
+++ file7_8.java
@@ -1,5 +1,5 @@
 
-public class file7 {
+public class file7_8 {
       private int firstMember;
       private int difference;
       public file7(int diff,int first) {
@@ -38,7 +38,25 @@ public class file7 {
     			  return num;
     		  else
     			  return 0;
-    	  }  	  
+    	  } 
+	public file7(int diff,int first) {
+  	  firstMember=first;
+  	  difference=diff;
+    }   
+    public boolean equal(int diff,int n,int first,int an)
+    {
+     if((firstMember==first)&&(difference==diff)&&(calculateN(an)==n)&&(calc(n)==an))
+     return true;
+     else
+     return false;
+    }
+    public boolean smalldiff(int diff)
+    {
+     if(diff<difference)
+     return true;
+     else
+     return false;
+    }
       }
 
 
@@ -46,7 +64,7 @@ public class file7 {
 public class tester {
 
 	public static void main(String[] args) {
-		file7 num=new file7(2,1);
+		file7_8 num=new file7_8(2,1);
 		num.print();
 		System.out.println(num.calc(25));
 		System.out.println(num.calculateN(2));				
